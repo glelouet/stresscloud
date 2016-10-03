b72c529236cd0c98f9b8d59ea998bbe80057c917
Guillaume Le Louët <guillaume.lelouet@gmail.com>
2016-10-03 12:55:29 +0200

    corrected bad import

--- StressCloud-common/src/test/java/fr/lelouet/stresscloud/control/LoadBaseTest.java
+++ StressCloud-common/src/test/java/fr/lelouet/stresscloud/control/LoadBaseTest.java
@@ -7,16 +7,16 @@ package fr.lelouet.stresscloud.control;
  * Copyright (C) 2012 - 2015 Mines de Nantes
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
@@ -24,20 +24,19 @@ package fr.lelouet.stresscloud.control;
 
 import java.util.Arrays;
 
-import junit.framework.Assert;
-
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * @author guillaume
- * 
+ *
  */
 public class LoadBaseTest {
 	@SuppressWarnings("unused")
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
-			.getLogger(LoadBaseTest.class);
+	.getLogger(LoadBaseTest.class);
 
 	@Test
 	public void testApply() {
